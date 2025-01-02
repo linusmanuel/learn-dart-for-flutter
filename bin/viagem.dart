9f4a673aef16d436c7a9dfdffee41f00768f072d
Joao Lino <linusmannuel@gmail.com>
2025-01-02 09:32:39 +0100

    03: refatoracao separando classes em arquivos

---
+++ bin/viagem.dart
@@ -0,0 +1,56 @@
+import 'transporte.dart';
+
+class Viagem {
+  static String codigoTrabalho = "Linus25";
+  double dinheiro = 0;
+  Transporte locomocao;
+  Set<String> registrosVisitados = <String>{};
+  Map<String, dynamic> registrarPrecos = {};
+
+  Viagem({required this.locomocao});
+
+  void printCodigoTrabalho() {
+    print("Linus25");
+  }
+
+  void  escolherMeioTransporte() {
+    switch (locomocao) {
+      case Transporte.carro:
+        print('Estou indo numa Aventura de Carro!');
+        break;
+      case Transporte.bicicleta:
+        print('Estou indo numa Aventura de Bike!');
+        break;
+      case Transporte.onibus:
+        print('Estou indo numa Aventura de Busão!');
+        break;
+      case Transporte.aviao:
+        print('Estou indo numa Aventura de Avião!');
+        break;
+      case Transporte.andando:
+        print('Estou indo numa Aventura a Pé!');
+        break;
+      case Transporte.helicoptero:
+        print('Estou indo numa Aventura de Helicóptero!');
+        break;
+      case Transporte.patins:
+        print('Estou indo numa Aventura de Patins!');
+        break;
+      case Transporte.skate:
+        print('Estou indo numa Aventura de Skate!');
+        break;
+      default:
+        {
+          print('Estou indo numa Aventura!');
+        }
+    }
+  }
+
+  void visitar(String localVisita) {
+    registrosVisitados.add(localVisita);
+  }
+
+  void registrarPrecoVisita(String local, dynamic preco) {
+    registrarPrecos[local] = preco;
+  }
+}
