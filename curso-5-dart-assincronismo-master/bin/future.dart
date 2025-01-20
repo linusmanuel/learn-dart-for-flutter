a0dd6bd82366795585af77e774523dd7c71bebbe
Joao Lino <linusmannuel@gmail.com>
2025-01-20 09:40:56 +0100

    2: aplicando future delayed

--- curso-5-dart-assincronismo-master/bin/future.dart
+++ curso-5-dart-assincronismo-master/bin/future.dart
@@ -4,5 +4,14 @@ void main() {
     return 24.5;
   }).then((value) => print('The value $value was returned!'));
 
+  Future.delayed(Duration(seconds: 5));
+
+  Future<int> myFutureFunc() async {
+    print('I have a function in the future');
+
+    await Future.delayed(Duration(seconds: 5));
+
+    return 12;
+  }
   print('Done with main!');
 }
