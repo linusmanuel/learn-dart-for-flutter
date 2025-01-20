01d96bf9e0cedb7e5b9422b2ff3a859ba6cfe61c
Joao Lino <linusmannuel@gmail.com>
2025-01-20 09:44:54 +0100

    2: Analisando codigo com future e delayed

--- curso-5-dart-assincronismo-master/bin/future.dart
+++ curso-5-dart-assincronismo-master/bin/future.dart
@@ -1,17 +1,34 @@
-void main() {
-  Future myFuture = Future(() {
-    print('Going back to the future');
-    return 24.5;
-  }).then((value) => print('The value $value was returned!'));
+// void main() {
+//   Future myFuture = Future(() {
+//     print('Going back to the future');
+//     return 24.5;
+//   }).then((value) => print('The value $value was returned!'));
+//
+//   Future.delayed(Duration(seconds: 5));
+//
+//   Future<int> myFutureFunc() async {
+//     print('I have a function in the future');
+//
+//     await Future.delayed(Duration(seconds: 5));
+//
+//     return 12;
+//   }
+//   print('Done with main!');
+// }
 
-  Future.delayed(Duration(seconds: 5));
-
-  Future<int> myFutureFunc() async {
-    print('I have a function in the future');
-
-    await Future.delayed(Duration(seconds: 5));
-
-    return 12;
-  }
-  print('Done with main!');
+void main(){
+  Future exterminador = Future(() async{
+    print('Venha comigo se quiser viver');
+    await Future.delayed(Duration(seconds: 1));
+    print('.');
+    await Future.delayed(Duration(seconds: 1));
+    print('.');
+    await Future.delayed(Duration(seconds: 1));
+    print('.');
+    await Future.delayed(Duration(seconds: 1));
+    print('.');
+    await Future.delayed(Duration(seconds: 1));
+    print('.');
+    return 'Hasta la vista, baby';
+  }).then((value) => print(value));
 }
