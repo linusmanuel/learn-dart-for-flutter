0a177b55124577223058d612f3254444ef5d5ce3
Joao Lino <linusmannuel@gmail.com>
2025-01-21 08:00:32 +0100

    2: Analisando erro e excecao

--- curso-5-dart-assincronismo-master/bin/future.dart
+++ curso-5-dart-assincronismo-master/bin/future.dart
@@ -1,34 +1,30 @@
-// void main() {
-//   Future myFuture = Future(() {
-//     print('Going back to the future');
-//     return 24.5;
-//   }).then((value) => print('The value $value was returned!'));
-//
-//   Future.delayed(Duration(seconds: 5));
-//
-//   Future<int> myFutureFunc() async {
-//     print('I have a function in the future');
-//
-//     await Future.delayed(Duration(seconds: 5));
-//
-//     return 12;
-//   }
-//   print('Done with main!');
-// }
+void main() {
+  // Future myFuture = Future(() {
+  //   print('Going back to the future');
+  //   return 24.5;
+  // }).then((value) => print('The value $value was returned!'));
+  //
+  // Future.delayed(Duration(seconds: 5));
 
-void main(){
-  Future exterminador = Future(() async{
-    print('Venha comigo se quiser viver');
-    await Future.delayed(Duration(seconds: 1));
-    print('.');
-    await Future.delayed(Duration(seconds: 1));
-    print('.');
-    await Future.delayed(Duration(seconds: 1));
-    print('.');
-    await Future.delayed(Duration(seconds: 1));
-    print('.');
-    await Future.delayed(Duration(seconds: 1));
-    print('.');
-    return 'Hasta la vista, baby';
-  }).then((value) => print(value));
+  Future<int> myFutureFunc(int a, int b) async {
+    try {
+      if(a>b) throw FormatException();
+
+      print('I have a function in the future');
+
+      await Future.delayed(Duration(seconds: 5));
+
+      return 12;
+    }
+    catch(e) {
+      print("An error occurred: $e");
+    }
+    finally {
+      print("The future is finally over!");
+      return 404;
+    }
+  }
+
+  myFutureFunc(7,4)
+    .then((value) => print("My function value is: $value"));
 }
