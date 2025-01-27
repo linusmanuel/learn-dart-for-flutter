eec3212fad65ab15db38c2ef1fc7a1cebdb93afe
Joao Lino <linusmannuel@gmail.com>
2025-01-27 09:53:10 +0100

    3: aplicando subscription

--- curso-5-dart-assincronismo-master/bin/streams.dart
+++ curso-5-dart-assincronismo-master/bin/streams.dart
@@ -1,16 +1,29 @@
+import 'dart:async';
+import 'dart:math';
+
 void main() {
   Stream myStream(int internal, [int? maxCount]) async* {
 
     int i = 1;
     while(i != maxCount) {
       print("Counting: $i");
-      Future.delayed(Duration(seconds: internal));
+      await Future.delayed(Duration(seconds: internal));
       yield i++;
     }
     print('The Stream is finished');
   }
 
   myStream(1, 10);
+
+  StreamSubscription mySubscriber = myStream(1,10).listen((event) {
+    if(event.isEven) {
+      print('This number is Even');
+    }
+  }, onError: (e) {
+    print("An error happend: $e");
+  }, onDone: () {
+    print("The subscriber is gone.");
+  });
 
   print('Main finished');
 
